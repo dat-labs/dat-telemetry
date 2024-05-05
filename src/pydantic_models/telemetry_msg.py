ae97bddb7fac604c73ab8fc456a6e6b8ca202428
Rijumone Choudhuri <rijumone.choudhuri@decision-tree.com>
2024-05-05 16:30:01 +0530

    run_id

--- src/pydantic_models/telemetry_msg.py
+++ src/pydantic_models/telemetry_msg.py
@@ -4,4 +4,5 @@ from dat_core.pydantic_models.dat_message import DatMessage
 
 class TelemetryMsg(BaseModel):
     connection_id: str
+    run_id: str
     dat_message: DatMessage
--- src/std_lib.py
+++ src/std_lib.py
@@ -3,8 +3,7 @@ from dat_client.rest import ApiException
 from dat_client.models.message import Message
 from dat_client.models.stack_trace import StackTrace
 from dat_client.configuration import Configuration
-from dat_client.models.dat_log_message import DatLogMessage
-from dat_client.models.dat_log_message_level import DatLogMessageLevel
+from dat_client.models.dat_message import DatMessage
 
 from .pydantic_models.telemetry_msg import TelemetryMsg
 
@@ -26,15 +25,16 @@ class DBTelemetryHandler(BaseTelemetryHandler):
         )
 
     def process_msg(self, telemetry_msg: TelemetryMsg) -> None:
-        telemetry_msg.dat_message.log.message = telemetry_msg.dat_message.log.model_dump_json()
+        # telemetry_msg.dat_message.log.message = telemetry_msg.dat_message.log.model_dump_json()
         with dat_client.ApiClient(self.configuration) as api_client:
             conn_run_log_api_instance = dat_client.ConnectionRunLogsApi(
                 api_client)
             try:
                 api_response = conn_run_log_api_instance.add_connection_run_log_connection_run_logs_post(
                     connection_id=telemetry_msg.connection_id,
-                    dat_log_message=DatLogMessage.from_json(
-                        telemetry_msg.dat_message.log.model_dump_json()
+                    run_id=telemetry_msg.run_id,
+                    dat_message=DatMessage.from_json(
+                        telemetry_msg.dat_message.model_dump_json()
                     ),
                 )
                 print(
